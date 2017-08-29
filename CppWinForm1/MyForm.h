20574d242d1582965449067f0504abe99eda7a5c
max-ostrom <max.ostrom@gmail.com>
2017-08-29 17:36:22 +0300

    event Click for ConvertButton

--- CppWinForm1/MyForm.h
+++ CppWinForm1/MyForm.h
@@ -61,30 +61,55 @@ namespace CppWinForm1 {
 		{
 			textField->Text = "";
 			XmlTextReader^ reader = gcnew XmlTextReader("1.xml");
-			while (reader->Read())
+			while (reader->ReadToFollowing("catalog"))
 			{
-				switch (reader->NodeType)
+				if (reader->ReadToDescendant("book"))
 				{
-				case XmlNodeType::Element:
-				{
-					textField->Text += "<" + reader->Name + " ";
-					while (reader->MoveToNextAttribute())
-						textField->Text += reader->Name + " = \"" + reader->Value + "\" ";
-					textField->Text += ">\r\n";
-					break;
-				}
-				case XmlNodeType::Text:
-				{
-					textField->Text += "    " + reader->Value + "\r\n";
-					break;
-				}
-				case XmlNodeType::EndElement:
-				{
-					textField->Text += "</" + reader->Name + ">\r\n";
-					break;
-				}
-				}
+					int pointCount = 0;
+					do
+					{
+						pointCount++;//Увеличиваем счетчик числа найденных точек
 
+						String^ author;
+						String^ title;
+						String^ Type;
+
+						//Считываем атрибут
+						Type = reader->GetAttribute("id");
+
+						//Внутри точки есть тэги <x> и <y>.
+						//Вытащим все тэги в отдельное дерево
+						XmlReader^ inner = reader->ReadSubtree();
+
+						//И пробежимся по всем внутренностям данного поддерева точки
+						while (inner->Read())
+						{
+							//Если текущий элемент это открывающий тэг <x>,
+							//то следующий элемент - это текст внутри тэга <x></x>
+							if ((inner->Name == "author") && (inner->NodeType == XmlNodeType::Element))
+							{
+								//Поэтому считываем еще один элемент и этот элемент то, что нам нужно
+								inner->Read();
+								author = inner->Value;
+							}
+							//Если текущий элемент это открывающий тэг <y>,
+							//то следующий элемент - это текст внутри тэга <y></y>
+							if ((inner->Name == "title") && (inner->NodeType == XmlNodeType::Element))
+							{
+								//Поэтому считываем еще один элемент и этот элемент то, что нам нужно
+								inner->Read();
+								title = inner->Value;
+							}
+						}
+
+						//Выведем считанную информацию 
+						textField->Text += "Книга №" + pointCount.ToString() + "\r\n";
+						textField->Text += "  ID = " + Type + "\r\n";
+						textField->Text += "  Author " + author + "\r\n";
+						textField->Text += "  Title " + title + "\r\n\n";
+
+					} while (reader->ReadToNextSibling("book"));
+				}
 			}
 		}
 		void LoadFileButton_click(Object^ sender, EventArgs^ e)
